32a8680455415ac5600446b146472957aeac09f1
paulahaertel <paula.haertel@mailbox.org>
2016-05-23 13:58:11 +0200

    commenting out code

--- src/Abgabe03/RationaleZahl.scala
+++ src/Abgabe03/RationaleZahl.scala
@@ -1,18 +1,18 @@
 case class RationaleZahl (zaehler: Int, nenner: Int) {
-  
-  require(nenner != 0, "Nenner darf nicht 0 sein!")
-  
-  def this (x: Int) = this(x, 1)
-  
-//  override def toString = zaehler + "/" + nenner
-  
-  override def toString =
-    zaehler + "/" + nenner
 
-  private ggT (a: Int, b: Int) =
-    
-    
-  private val kuerz_mich = ggT(zaehler, nenner)
+//  require(nenner != 0, "Nenner darf nicht 0 sein!")
+//
+//  def this(x: Int) = this(x, 1)
+//
+//  //  override def toString = zaehler + "/" + nenner
+//
+//  override def toString =
+//    zaehler + "/" + nenner
+//
+//  private ggT(a: Int, b: Int) =
+//    println(a + "/" + b)
+//
+//
+//  private val kuerz_mich = ggT(zaehler, nenner)
 
 }
-
